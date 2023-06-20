b95044521e9e51a54ec90455cd4a4834d00c55ba
jaffa <jari.jaanto@gmail.com>
2023-06-20 09:25:40 +0300

    increase version number

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ellie',
-      version='1.0',
+      version='1.1.0',
       description='Ellie API database connectors',
       author='Ellie Technologies',
       url='https://www.ellie.ai/',
